1a9214da6f825e175386aa91df88a24c9462e11d
Cjescalona1 <cjescalona1@gmail.com>
2020-12-20 06:52:10 -0400

    serie detail

--- m-fe/src/app/components/serie/serie.component.ts
+++ m-fe/src/app/components/serie/serie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviedbService } from '../../../services/moviedb.service';
+import { Router } from '@angular/router'; 
  
 @Component({
   selector: 'app-serie',
@@ -15,7 +16,8 @@ export class SerieComponent {
   loadingSerie: boolean;
 
   constructor(private router: ActivatedRoute,
-    private moviedb: MoviedbService) {
+    private moviedb: MoviedbService,
+    private rout: Router) {
 
     this.loadingSerie = true;
     this.router.params.subscribe(params => { 
@@ -26,7 +28,6 @@ export class SerieComponent {
         }) 
     })
 
-  }
-
+  } 
 }
 
